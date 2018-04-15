f900aba16353c4dd908f06e0f5d824de21375e0f
Hideki Terashima <terahide27@Hideki-no-MacBook-Air.local>
2018-04-15 11:44:30 +0900

    change more RESTfull(provisional implementation)

--- src/main/java/hello/rest/RestGreetingController.java
+++ src/main/java/hello/rest/RestGreetingController.java
@@ -1,11 +1,16 @@
 package hello.rest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import hello.Greeting;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class RestGreetingController {
@@ -14,8 +19,21 @@ public class RestGreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/rest/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    public List<Greeting> greeting() {
+        List<Greeting> greetings = new ArrayList<>();
+        greetings.add(new Greeting(counter.incrementAndGet(),
+                String.format(template, "world")));
+        return greetings;
+    }
+
+    @RequestMapping("/rest/greeting/{id}")
+    public Greeting greeting(@PathVariable long id) {
+        return new Greeting(id,
+                String.format(template, "world"));
+    }
+
+    @RequestMapping(value = "/rest/greeting", method=POST)
+    public String greetingSubmit(@Validated Greeting greeting) {
+        return "{ \"result\" : \"success\" }";
     }
 }
