d205353ed76df7bc059c85e83e0576b6e4dfcadc
Hideki Terashima <terahide27@Hideki-no-MacBook-Air.local>
2018-04-15 11:47:49 +0900

    add transactional

--- src/main/java/hello/GreetingService.java
+++ src/main/java/hello/GreetingService.java
@@ -1,7 +1,13 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,20 +15,32 @@ import java.util.stream.Collectors;
 
 @Service
 public class GreetingService {
-    private final static Map<Long,Greeting> greetings = new LinkedHashMap<>();
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Transactional
     public void save(Greeting greeting){
-        greetings.put(greeting.getId(), greeting);
+        jdbcTemplate.update("insert into greetings (id, content) values (?, ?)",greeting.getId(),greeting.getContent());
     }
 
     public List<Greeting> all(){
-        return greetings.values().stream().collect(Collectors.toList());
+        return jdbcTemplate.query("select * from greetings",(rs, rowNum) -> toGreeting(rs));
+    }
+
+    private Greeting toGreeting(ResultSet rs) {
+        try {
+            return new Greeting(rs.getLong("ID"), rs.getString("CONTENT"));
+        }catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Greeting get(long id){
-        if( ! greetings.containsKey(id)){
+        try {
+            return jdbcTemplate.queryForObject("select * from greetings where id = ?", (rs, rowNum) -> toGreeting(rs), id);
+        }catch(EmptyResultDataAccessException e){
             throw new ResourceNotFoundException();
         }
-        return greetings.get(id);
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+drop table Greetings if exists;
+create table Greetings(ID number(10), content varchar(30) NOT NULL);
