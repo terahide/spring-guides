e92417e434789db63719acf689ed684d844d3893
Hideki Terashima <terahide27@Hideki-no-MacBook-Air.local>
2018-04-15 11:43:22 +0900

    add validation

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,8 +1,15 @@
 package hello;
 
+import javax.validation.constraints.*;
+
 public class Greeting {
 
+    @NotNull
+    @Min(0)
     private long id;
+
+    @NotNull
+    @Size(min=2, max=30)
     private String content;
 
     public long getId() {
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,8 +2,9 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,7 +19,11 @@ public class GreetingController {
     }
 
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+    public String greetingSubmit(@Validated Greeting greeting, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "greeting";
+        }
         return "result";
     }
 }
