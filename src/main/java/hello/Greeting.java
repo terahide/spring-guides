4403334b55bd49483b3d9b719a3688c2b861f288
Hideki Terashima <terahide27@Hideki-no-MacBook-Air.local>
2018-04-15 11:43:54 +0900

    add RESTfull

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -12,6 +12,15 @@ public class Greeting {
     @Size(min=2, max=30)
     private String content;
 
+    public Greeting(){
+        super();
+    }
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
     public long getId() {
         return id;
     }
